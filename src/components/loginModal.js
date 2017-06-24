9338649b21541dbef777047bd8cf312c825f85a5
Tanuj <tgirish10@gmail.com>
2017-06-24 00:29:23 -0500

    sign in modal

---
+++ src/components/loginModal.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class LoginModal extends Component {
+  render() {
+    return (
+      <div id="myModal" className="modal fade" role="dialog">
+        <div className="modal-dialog">
+        </div>
+      </div>
+    );
+  }
+}
